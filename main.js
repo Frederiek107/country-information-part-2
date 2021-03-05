9ce02d63c1eed09ae89bb7318279c537489690a3
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-03-05 21:11:14 +0100

    bonus assignments added

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ input.addEventListener("keyup", showDataKeyboard);
 const page= document.getElementById("mainpage");
 
 function returnURL() {
-    let inputValue = input.value;
-    let apiURL = "https://restcountries.eu/rest/v2/name/" + inputValue + "?fullText=true";
+    let apiURL = "https://restcountries.eu/rest/v2/name/" + input.value + "?fullText=true";
     return apiURL;
 }
 
@@ -20,60 +19,65 @@ function showDataKeyboard(e) {
     }
 }
 
+// Returns a string with the subregion and population
 async function fetchData() {
     const endpoint = await axios.get(returnURL());
-    const result = endpoint.data[0].name + " is situated in " + endpoint.data[0].subregion + ". "
-            + "It has a population of " + endpoint.data[0].population + " people."
+    const {name, subregion, population} = endpoint.data[0];
+    const result = name +  " is situated in " + subregion + ". It has a population of " + population.toLocaleString() + " people."
     return result;
     }
 
+// Returns a string with the capital and currency
 async function fetchCurrencies() {
     const endpoint = await axios.get(returnURL());
-    let string = "The capital is " + endpoint.data[0].capital + " and you can pay with ";
-    const currencies = endpoint.data[0].currencies;
+    const {capital, currencies} = endpoint.data[0];
+    let string = "The capital is " + capital + " and you can pay with ";
     for (const currency of currencies) {
         if (currency === currencies[0]) {
-            string = string + (currency.name) + "'s.";
+            string = string + currency.name + "'s.";
         } else if (currency > currencies[0]) {
-            string = string + " and " + (currency.name) + "'s."
+            string = string + " and " + currency.name + "'s."
         }
     }
     return string;
 }
 
+// Returns a string with the language(s)
 async function fetchLanguages() {
     const endpoint = await axios.get(returnURL());
-    let string = "The people from " + endpoint.data[0].name + " speak ";
-    const languages = endpoint.data[0].languages;
+    const {name, languages} = endpoint.data[0];
+    let string = "The people from " + name + " speak ";
     for (const language of languages) {
-        if (language === languages[0] && languages.length === 1) {
+        if (languages.length === 1) {
             string = string + language.name + ".";
         }
         if (language === languages[0] && languages.length > 1) {
             string = string + language.name;
         }
-        if (languages.length === 2 && language === languages[1]) {
+        if (language === languages[1] && languages.length === 2) {
             string = string + " and " + language.name + ".";
         }
-        if (languages.length > 2 && language !== languages[0] && language !== languages[languages.length - 1]) {
+        if (language !== languages[0] && language !== languages[languages.length-1] && languages.length > 2) {
             string = string + ", " + language.name;
         }
-        if (languages.length > 2 && language === languages[languages.length - 1]) {
+        if (language === languages[languages.length - 1] && languages.length > 2) {
             string = string + " and " + language.name + ".";
         }
     }
     return string;
 }
 
+// Returns an image-element with the flag
 async function fetchFlag() {
     const endpoint = await axios.get(returnURL());
     const flagItem = document.createElement("img");
-    const country = endpoint.data[0];
-    flagItem.setAttribute("src", country.flag);
+    const {flag} = endpoint.data[0];
+    flagItem.setAttribute("src", flag);
     flagItem.setAttribute("id", "flag");
     return flagItem;
 }
 
+// Shows the combined results on the page
 async function showResults() {
     try {
         const endpoint = await axios.get(returnURL());
--- worldmap.js
+++ worldmap.js
@@ -1,57 +1,75 @@
 const countries = document.getElementById("countries");
+countries.setAttribute("id", "countries")
 
-async function fetchData(){
-    const apiURL = await axios.get("https://restcountries.eu/rest/v2/all");
-    console.log(apiURL);
-}
-fetchData()
 
+//Returns an array with all countries, sorted by population
 async function sortByPopulation() {
     const API = await axios.get("https://restcountries.eu/rest/v2/all");
-    const countries=API.data;
+    console.log(API);
     const arrayPopulations=[];
-    for (country of countries) {
-        const name = country.name;
-        const flag = country.flag;
-        const population = country.population;
-        const region= country.region;
+    for (let country of API.data) {
+        const {name, flag, population, region} = country;
         arrayPopulations.push({name,flag,population,region});
     }
     arrayPopulations.sort((a,b) => {
         return a.population-b.population;
     });
-    console.log(arrayPopulations);
     return arrayPopulations;
 }
 
+//Shows all countries on the page in corresponding colors
 async function showCountries() {
     const countriesList = await sortByPopulation();
-        for (country of countriesList) {
+        for (let country of countriesList) {
             const flag = document.createElement("img");
             flag.setAttribute("src", country.flag);
-            flag.setAttribute("id", "flagmap")
-            newItem = document.createElement("div");
-            newItem.setAttribute("id", "worldmap-item");
+            flag.setAttribute("id", "flagmap");
+            const newItem = document.createElement("div");
+            const newItemPopulation = document.createElement("div");
+            newItemPopulation.setAttribute("id", "population-"+(country.name));
             newItem.append(flag, country.name);
-            if (country.region==="Africa") {
-                newItem.style.setProperty("color", "#5575c1");
-            }
-            else if (country.region==="Americas") {
-                newItem.style.setProperty("color", "#4d824b");
-            }
-            else if (country.region==="Asia") {
-                newItem.style.setProperty("color", "#d04e5b");
-            }
-            else if (country.region==="Europe") {
-                newItem.style.setProperty("color", "#ffd435");
-            }
-            else if (country.region==="Oceania") {
-                newItem.style.setProperty("color", "#a653ba");
-            }
-            else {
-                newItem.style.setProperty("color", "white");
+            switch (country.region) {
+                case "Africa":
+                    newItem.style.setProperty("color", "#5575c1");
+                    break;
+                case "Americas":
+                    newItem.style.setProperty("color", "#4d824b");
+                    break;
+                case "Asia":
+                    newItem.style.setProperty("color", "#d04e5b");
+                    break;
+                case "Europe":
+                    newItem.style.setProperty("color", "#ffd435");
+                    break;
+                case "Oceania":
+                    newItem.style.setProperty("color", "#a653ba");
+                    break;
+                default:
+                    newItem.style.setProperty("color", "white");
             }
             countries.appendChild(newItem);
+            countries.appendChild(newItemPopulation);
         }
+    countries.addEventListener("click", showPopulation);
 }
 showCountries()
+
+// Shows the population below clicked country.
+async function showPopulation(e) {
+    const clickedCountry = e.srcElement.innerText;
+    console.log(clickedCountry);
+    const ID="population-"+clickedCountry;
+    console.log(ID);
+    const clickedElement = document.getElementById("population-"+clickedCountry);
+    clickedElement.style.setProperty("color", "goldenrod");
+    if (clickedElement.hasChildNodes()){
+        clickedElement.removeChild(clickedElement.childNodes[0]);
+        return;
+    }
+    //fetch population
+    const countriesList = await sortByPopulation();
+    const result = countriesList.filter(country=> {
+    return country.name===clickedCountry})
+    //append info
+    clickedElement.textContent="Population: "+result[0].population.toLocaleString();
+}
